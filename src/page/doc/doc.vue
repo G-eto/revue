279042d131dd7cc43611a8c1d2c8ff311322f152
lcp <1340878220@qq.com>
2021-01-28 19:20:55 +0800

    feat:change port

--- src/page/doc/doc.vue
+++ src/page/doc/doc.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
     documents
+    {{ioc}}
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-name: "doc"
+  name: "doc",
+  data() {
+    return {
+      ioc: null
+    }
+  },
+  mounted () {
+    axios({
+      method: 'get',
+      url: 'http://localhost:8080/v1/test',
+    })
+        .then(response => (this.ioc = response.data.message))
+        .catch(function (error) { // 请求失败处理
+          console.log(error);
+        });
+  },
 }
+
+
 </script>
 
 <style scoped>
